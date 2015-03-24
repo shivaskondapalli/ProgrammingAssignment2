3ad18c22b90f681ed5ff2a476985dd07a21566e8
shivaskondapalli <shivaskondapalli@gmail.com>
2015-03-24 13:42:58 -0600

    R Programming: Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,78 @@
-## Put comments here that give an overall description of what your
-## functions do
+######################################################################
+## This program is run in R console and in three parts.
+##
+##      Part 1: Create a matrix
+##      > xx <- rnorm(16)       # Generate 16 random normal variates
+##      > dim(xx) <- c(4, 4)    # Use dim to make it a square matrix
+##
+##      Part 2: Create our special matrix
+##      > x <- makeCacheMatrix(xx)      # This creates our special matrix
+##
+##      Part 3: Compute inverse of our matrix
+##      > cacheSolve(x)
+##
+##      First time, when cacheSolve(x) is called, it uses
+##      solve() to compute the inverse and caches using
+##      x$setInvMatrix() method, that is implemented inside
+##      makeCacheMatrix().
+##
+##      After this the subsequent runs of cacheSolve(x)
+##      will fetch the cached matrix displaying a message
+##      "Getting cached matrix"
+######################################################################
 
-## Write a short comment describing this function
 
+
+
+###########################################################
+##
+##      Function:       makeCacheMatrix
+##      Arguments:      A square matrix
+##      Returns:        A list of methods or functions
+##                      implemented inside this function
+###########################################################
 makeCacheMatrix <- function(x = matrix()) {
 
+        invMat <- NULL
+
+        getMatrix <- function() {
+                return(x)
+        }
+
+        getInvMatrix <- function() {
+                return(invMat)
+        }
+
+        setInvMatrix <- function(invertedMat) {
+                return(invMat <<- invertedMat)
+        }
+
+        list(getMatrix = getMatrix,
+             getInvMatrix = getInvMatrix,
+             setInvMatrix = setInvMatrix)
 }
 
 
-## Write a short comment describing this function
-
+###########################################################
+##      Function:       cacheSolve
+##      Arguments:      Arbitrary. Coerces to the type of
+##                      passed argument
+##      Returns:        An inverse matrix
+##
+###########################################################
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        dataMatrix <- NULL
+
+        localInvMat <- x$getInvMatrix()
+
+        if(!is.null(localInvMat)) {
+                message("Getting cached matrix")
+                return(localInvMat)
+        }
+
+        dataMatrix <- x$getMatrix()
+        localInvMat <- (solve(dataMatrix) %*% dataMatrix)
+        x$setInvMatrix(localInvMat)
+        return(localInvMat)
 }
